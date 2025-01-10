4a41718181268a33a7f731dcf66941f9559f0b03
Israa Chaabi <ichaabi@e2r7p8.1337.ma>
2025-01-10 23:13:13 +0100

    ex02 done

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ichaabi <ichaabi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 21:46:05 by ichaabi           #+#    #+#             */
-/*   Updated: 2025/01/10 21:46:21 by ichaabi          ###   ########.fr       */
+/*   Updated: 2025/01/10 23:12:08 by ichaabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,16 @@
 
 int main()
 {
-	
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;//pointer vers str
+	std::string& stringREF = str;//ref vers str
+
+	std::cout << "Memory address of str: " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+	return (0);
 }
--- t.cpp
+++ t.cpp
@@ -1,19 +1,53 @@
 
 #include <iostream>
 
+// int main()
+// {
+// 	int numberofballs = 42;
+// 	int* ballsPtr = &numberofballs;
+// 	int& ballsRef = numberofballs;//je ne peux pas changer sur quoi ma reference pointe
+// 	// int& ballsRef2;//faux c est une reference qui ne pointe sur rien
+
+// 	std::cout << numberofballs << " " << *ballsPtr << " " << ballsRef << " "
+// 		<< &numberofballs << " " << ballsPtr << std::endl;
+
+// 	*ballsPtr = 21;
+// 	std::cout << numberofballs << std::endl;
+// 	ballsRef = 84;
+// 	std::cout << numberofballs << std::endl;
+// 	return 0;
+// }
+
+//--------==============------------------
+
+
+void	byPtr(std::string* str)
+{
+	*str += " and ho";
+}
+
+void	byConstPtr(std::string const * str)
+{
+	std::cout << *str << std::endl;
+}
+
+void	byRef(std::string& str)
+{
+	str += " and ouis";
+}
+
+void	byConstRef(std::string const & str)
+{
+	std::cout << str << std::endl;
+}
+
 int main()
 {
-	int numberofballs = 42;
-	int* ballsPtr = &numberofballs;
-	int& ballsRef = numberofballs;//je ne peux pas changer sur quoi ma reference pointe
-	// int& ballsRef2;//faux c est une reference qui ne pointe sur rien
+	std::string str = " i love robio";
 
-	std::cout << numberofballs << " " << *ballsPtr << " " << ballsRef << " "
-		<< &numberofballs << " " << ballsPtr << std::endl;
+	std::cout << str << std::endl;
+	byPtr(&str);
+	byConstPtr(&str);
 
-	*ballsPtr = 21;
-	std::cout << numberofballs << std::endl;
-	ballsRef = 84;
-	std::cout << numberofballs << std::endl;
-	return 0;
+	str = "i love me";
 }
